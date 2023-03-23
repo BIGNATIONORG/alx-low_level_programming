69a7f21f1060a6c0ab2c3b2d87115f9c581f06df
BIGNATIONORG <nzokapeter00@gmail.com>
2023-03-23 11:41:32 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - test function that prints if integeris positive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+
+{
+	int i;
+
+	i = 0;
+	positive or negative(i);
+
+	return (0);
+}
