d3ffddc6fde36c6088eff82ccdadabbdb0729f7b
BIGNATIONORG <nzokapeter00@gmail.com>
2023-05-08 10:29:00 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,29 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * read_file - Reads a file and prints its contents to the console
- * @filename: Name of the file to be read
- * @max_chars: Maximum number of characters to read from the file
- * Return: Number of characters read and printed to the console
- *         0 if the function fails or if the filename is NULL.
+ * read_textfile- Read the text file print to STDOUT.
+ * @filename: The text file to be read
+ * @letters: number of the letters to be read
+ * Return: w- actual number of the bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-size_t read_file(const char *filename, size_t max_chars)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buffer;
-    FILE *file;
-    size_t read_count, write_count;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-    if (filename == NULL)
-        return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-    file = fopen(filename, "r");
-    if (file == NULL)
-        return (0);
-
-    buffer = malloc(max_chars + 1);
-    if (buffer == NULL)
-    {
-        fclose(file);
-        return (0);
-    }
-
-    read_count = fread(buffer, sizeof(char), max_chars, file);
-    write_count = fwrite(buffer, sizeof(char), read_count, stdout);
-
-    free(buffer);
-    fclose(file);
-
-    return write_count;
+	free(buf);
+	close(fd);
+	return (w);
 }
+
